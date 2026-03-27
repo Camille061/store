b841e372431b29c15901a41cb77ab3e67e663438
Camille061 <slideshort72@gmail.com>
2026-03-27 23:43:30 +0700

    icon document

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let exampleBtn3 = document.getElementById("example-btn3")
 
 
 
-
+exampleBtn1.onclick = ()=> {
+    window.location.href = ""
+}
 
 exampleBtn2.onclick = ()=> {
     window.location.href = "https://drive.google.com/drive/folders/1RYq8n-8B7Rhy6jsDekK1vxTUO6XBH3D0?usp=sharing"
