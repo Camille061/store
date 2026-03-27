249304f6931c8dd19a97d9843cc6d6e6089a0eea
Camille061 <slideshort72@gmail.com>
2026-03-27 22:19:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let exampleBtn1 = document.getElementById("example-btn1")
+let exampleBtn2 = document.getElementById("example-btn2")
+let exampleBtn3 = document.getElementById("example-btn3")
+
+
+
+
+
+exampleBtn2.onclick = ()=> {
+    window.location.href = "https://drive.google.com/drive/folders/1RYq8n-8B7Rhy6jsDekK1vxTUO6XBH3D0?usp=sharing"
+}
+exampleBtn3.onclick = ()=> {
+    window.location.href = "https://drive.google.com/drive/folders/1cOZEjgCCovEnrS4W4xXkZl2GtEuPCd2x?usp=sharing"
+}
